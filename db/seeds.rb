e8a25ff0d478996a0fa69da4244f1bda8cf4528d
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-02-24 19:44:17 -0300

    WATCH LIST / UNFINISHED

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Movie.create(title: "Lord of Ther Rings: The Fellowship of the Ring", overview: "Best movies in the world", poster_url: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg", rating: 9.8)
+Movie.create(title: "Lord of Ther Rings: The Two Towers", overview: "Best movies in the world", poster_url: "https://www.oficialhostgeek.com.br/wp-content/uploads/2020/12/zzzz-scaled.jpg", rating: 9.9)
+Movie.create(title: "Lord of Ther Rings: The Return of the King", overview: "Best movies in the world", poster_url: "https://upload.wikimedia.org/wikipedia/pt/0/0d/EsdlaIII.jpg", rating: 10.0)
